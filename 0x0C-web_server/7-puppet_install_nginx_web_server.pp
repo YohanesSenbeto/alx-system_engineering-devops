c80697121eb3860cd3b6e95a8f6ead608f41cd28
YohanesSenbeto <jonicasenbeto@gmail.com>
2023-11-28 07:27:36 -0800

    Last updated of Webserver files

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,54 +1,6 @@
-# Install Nginx package
+# Installs the Nginx server
 
-package { 'nginx':
-  ensure => installed,
-}
-
-# Configure and start Nginx service
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Create the index.html file with 'Hello World!' content
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-# Configure the Nginx default site
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-    # Nginx default site configuration
-
-    server {
-        listen 80;
-        server_name _;
-
-        location / {
-            return 301 http://$host/redirect_me;
-        }
-
-        location /redirect_me {
-            return 301 http://$host/;
-        }
-
-        location / {
-            root /var/www/html;
-            index index.html;
-        }
-    }
-  ",
-  notify  => Service['nginx'],
-  require => Package['nginx'],
-}
-
-# Execute curl command to test Nginx server
-exec { 'test_nginx':
-  command     => "curl -s http://localhost/",
-  path        => ['/usr/bin', '/bin'],
-  refreshonly => true,
-  subscribe   => Service['nginx'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/lexxyla permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
